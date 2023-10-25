3084dbb0627bebb9448e9eb8afd6ded44f2fdd4f
Jules <jules.holder.54@gmail.com>
2023-10-25 10:29:27 +0200

    soirée et tarif DTO

---
+++ nrv.api/src/dto/SoireeDTO.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace nrv\api\dto;
+
+class SoireeDTO extends DTO
+{
+    public int $id;
+    public string $nom;
+    public ThemeDTO $theme;
+    public string $date;
+    public LieuDTO $lieu;
+    public TarifDTO $tarif;
+    public array $spectacles;
+
+    public function __construct(int $id, string $nom, ThemeDTO $theme, string $date, LieuDTO $lieu, TarifDTO $tarif, array $spectacles)
+    {
+        $this->id = $id;
+        $this->nom = $nom;
+        $this->theme = $theme;
+        $this->date = $date;
+        $this->lieu = $lieu;
+        $this->tarif = $tarif;
+        $this->spectacles = $spectacles;
+    }
+}
