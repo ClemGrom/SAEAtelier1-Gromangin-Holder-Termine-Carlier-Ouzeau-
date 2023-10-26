ac6597c02e09d20ef662f63ac640796dc58a5f74
Jules <jules.holder.54@gmail.com>
2023-10-26 14:51:06 +0200

    routes fonctionnelles

--- nrv.api/src/config/routes.php
+++ nrv.api/src/config/routes.php
@@ -1,10 +1,14 @@
 <?php
 
+use nrv\api\actions\GetSoireeAPIAction;
+use nrv\api\actions\GetSpectacleAPIAction;
 use nrv\api\actions\GetSpectaclesAPIAction;
 use Slim\App;
 
 return function (App $app):void{
 
     $app->get('/spectacles[/]', GetSpectaclesAPIAction::class)->setName('spectacles');
+    $app->get('/spectacles/{id}[/]', GetSpectacleAPIAction::class)->setName('spectacle');
+    $app->get('/soiree/{id}[/]', GetSoireeAPIAction::class)->setName('soiree');
 
 };
