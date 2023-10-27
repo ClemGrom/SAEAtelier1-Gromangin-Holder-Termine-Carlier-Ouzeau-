ec133befe604ba52546b56bcc85b7c8df01f9ab1
Jules <jules.holder.54@gmail.com>
2023-10-27 10:28:58 +0200

    ajout attribut commande_actuelle dans l'entité utilisateur et dans le dto utiisateur

--- nrv.api/src/dto/UtilisateurDTO.php
+++ nrv.api/src/dto/UtilisateurDTO.php
@@ -11,8 +11,9 @@ class UtilisateurDTO extends DTO
     public bool $admin;
     public array $commandes;
     public string $password;//Doit être utilisé que pour le transit |ACTION->SERVICE|(ex creation user) ET !SURTOUT! PAS |SERVICE->ACTION|(ex: listage user, mdp pas important + doit être null)
+    public string $commande_actuelle;
 
-    public function __construct(string $nom, string $prenom, string $email, bool $admin = null, array $commandes = null,string $uuid = null, string $password=null)
+    public function __construct(string $nom, string $prenom, string $email, bool $admin = null, array $commandes = null, string $uuid = null, string $password = null, string $commande_actuelle = null)
     {
         $this->uuid = $uuid;
         $this->nom = $nom;
@@ -21,5 +22,6 @@ class UtilisateurDTO extends DTO
         $this->admin = $admin;
         $this->commandes = $commandes;
         $this->password = $password;
+        $this->commande_actuelle = $commande_actuelle;
     }
 }
--- nrv.api/src/entities/Utilisateur.php
+++ nrv.api/src/entities/Utilisateur.php
@@ -3,6 +3,7 @@
 namespace nrv\api\entities;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use nrv\api\dto\UtilisateurDTO;
 
@@ -11,11 +12,16 @@ class Utilisateur extends Model
     protected $table = "utilisateur";
     protected $primaryKey = "uuid";
     protected $keyType = "string";
-    protected $fillable = ["nom", "prenom", "email", "password", "admin"];
+    protected $fillable = ["nom", "prenom", "email", "password", "admin","commande_actuelle"];
 
     public function commandes() : HasMany
     {
         return $this->hasMany(Commande::class, 'id_utilisateur');
+    }
+
+    public function commandeActuelle(): belongsTo
+    {
+        return $this->belongsTo(Commande::class, 'commande_actuelle');
     }
 
     public function toDTO() : UtilisateurDTO {
