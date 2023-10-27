16731586c90bfc474d2d314f95c1ed6e25397049
Jules <jules.holder.54@gmail.com>
2023-10-27 14:40:26 +0200

    action création user

--- nrv.api/src/actions/GetCreationUtilisateurAction.php
+++ nrv.api/src/actions/GetCreationUtilisateurAction.php
@@ -2,6 +2,7 @@
 
 namespace nrv\api\actions;
 
+use nrv\api\renderer\JSONRenderer;
 use nrv\api\services\CsrfService;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -17,6 +18,8 @@ class GetCreationUtilisateurAction
 
     public function __invoke(ServerRequestInterface $rq, ResponseInterface $rs, array $args): ResponseInterface
     {
+        $data = ['token' => $this->csrfService::generate()];
+        return JSONRenderer::render($rs, 200, $data);
 
     }
 }
