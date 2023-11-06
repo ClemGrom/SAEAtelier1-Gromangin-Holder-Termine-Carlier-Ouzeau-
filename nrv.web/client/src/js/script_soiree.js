90b0b9058f85b6ee4a06df4de9733635fae91217
Maxime48 <37154360+Maxime48@users.noreply.github.com>
2023-11-06 10:01:37 +0100

    Update script_soiree.js

--- nrv.web/client/src/js/script_soiree.js
+++ nrv.web/client/src/js/script_soiree.js
@@ -4,8 +4,8 @@ import { getSoirees, getSoireeById } from "./soiree.js";
 
 var url = new URL(window.location.href);
 var search_params = new URLSearchParams(url.search); 
-if(search_params.has('soiree')) {
-  var id = search_params.get('soiree');
+if(search_params.has('id')) {
+  var id = search_params.get('id');
 }
 
 console.log(id);
